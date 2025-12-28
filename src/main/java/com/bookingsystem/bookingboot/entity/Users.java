d126b62dbdd57c91ac604aec15e0467601ffa9e5
scott <fffff40103@gmail.com>
2025-12-28 22:36:23 +0800

    [IMP]拿掉多餘註解、加入Authority屬性

--- src/main/java/com/bookingsystem/bookingboot/entity/Users.java
+++ src/main/java/com/bookingsystem/bookingboot/entity/Users.java
@@ -14,36 +14,6 @@ public class Users {
     private String phoneNumber;
     private Date createdAt;
     private Date updatedAt;
+    private String authority;
 
-//    // getter / setter 全部寫出來
-//
-//    public String getId() { return id; }
-//    public void setId(String id) { this.id = id; }
-//
-//    public String getUserName() { return userName; }
-//    public void setUserName(String userName) { this.userName = userName; }
-//
-//    public String getEmail() { return email; }
-//    public void setEmail(String email) { this.email = email; }
-//
-//    public String getPasswordHash() { return passwordHash; }
-//    public void setPasswordHash(String passwordHash) { this.passwordHash = passwordHash; }
-//
-//    public String getPhoneNumber() { return phoneNumber; }
-//    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
-//
-//    public Date getCreatedAt() { return createdAt; }
-//    public void setCreatedAt(Date createdAt) { this.createdAt = createdAt; }
-//
-//    public Date getUpdatedAt() { return updatedAt; }
-//    public void setUpdatedAt(Date updatedAt) { this.updatedAt = updatedAt; }
-//
-//    @Override
-//    public String toString() {
-//        return "Users{" +
-//                "id='" + id + '\'' +
-//                ", userName='" + userName + '\'' +
-//                ", email='" + email + '\'' +
-//                '}';
-//    }
 }
